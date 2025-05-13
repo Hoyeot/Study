8330be03f2e91bc9b3c3f7ba7e2cca0da0fc8f8d
Hoyeot <hycool72@gmail.com>
2025-05-13 14:46:14 +0900

    Update player.cpp

--- 2DGame/Server/player.cpp
+++ 2DGame/Server/player.cpp
@@ -1,7 +1,44 @@
 #include <iostream>
 #include "player.h"
 
+Player::Player(SOCKET socket, const char* id, float currentHp, float x, float y)
+	: socket(socket), id(id), x(x), y(y), currentHp(currentHp)
+{
+	
+}
+
 Player::~Player()
 {
 
+}
+
+SOCKET Player::GetSocket() const
+{
+	return socket;
+}
+
+void Player::SetPosition(float x, float y)
+{
+	this->x = x;
+	this->y = y;
+}
+
+float Player::GetHp()
+{
+	return currentHp;
+}
+
+float Player::GetX()
+{
+	return x;
+}
+
+float Player::GetY()
+{
+	return y;
+}
+
+const char* Player::GetId()
+{
+	return id;
 }
